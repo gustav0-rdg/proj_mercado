05301fda191dcf26d10b6e91880829862f63a58e
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-06-10 10:27:43 -0300

    fix: const to let

--- static/js/pag_carrinho/get-itens.js
+++ static/js/pag_carrinho/get-itens.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', async function(){
     const data = await fetch("https://disco-magico.onrender.com/carrinho");
-    data = await data.json()
     console.log(data);
     if(data){
-        const itensList = document.querySelector(".carrinho__itens");
+        let itensList = document.querySelector(".carrinho__itens");
         data.forEach(element => {
             itensList.innerHTML = `${element.nome_filme}`;
         });
