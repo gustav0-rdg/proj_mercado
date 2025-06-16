68f359d452f4e85dc5bdf2a7f427252c106340ab
gustavo rodrigues <gustavolourenc821@gmail.com>
2025-06-16 16:28:13 -0300

    fix: bugs

--- data/conexao.py
+++ data/conexao.py
@@ -4,10 +4,10 @@ class Connection():
 
     def create():
         connection = mysql.connector.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="root",
+            host="disco-magico-gustavor0d-disco-magico-gustavo.c.aivencloud.com",
+            port=13935,
+            user="avnadmin",
+            password="AVNS_UjqarbEi4gwkBWadktR",
             database="db_mercado"
         )
         return connection
