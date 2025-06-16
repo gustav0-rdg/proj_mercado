2766dea924a54ac6de70aa5ba68ae4963f600ae4
Gustavo <gustavolourenco821@gmail.com>
2025-06-16 19:07:16 -0300

    add: database correta

--- data/conexao.py
+++ data/conexao.py
@@ -2,23 +2,12 @@ import mysql.connector
 
 class Connection():
 
-    # def create():
-    #     connection = mysql.connector.connect(
-    #         host="disco-magico-gustavor0d-disco-magico-gustavo.c.aivencloud.com",
-    #         port=13935,
-    #         user="avnadmin",
-    #         password="AVNS_UjqarbEi4gwkBWadktR",
-    #         database="db_mercado"
-    #     )
-    #     return connection
-    
     def create():
         connection = mysql.connector.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="root",
+            host="disco-magico-gustavor0d-disco-magico-gustavo.c.aivencloud.com",
+            port=13935,
+            user="avnadmin",
+            password="AVNS_UjqarbEi4gwkBWadktR",
             database="db_mercado"
         )
         return connection
-    
